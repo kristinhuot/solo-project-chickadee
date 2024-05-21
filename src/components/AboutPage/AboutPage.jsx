92d88d72a348cc14ca8bc5c1cb2554201f0c57ee
Kristin Huot <kristin.huot@gmail.com>
2024-05-21 13:52:54 -0500

    began work on MyNest component

--- src/components/AboutPage/AboutPage.jsx
+++ src/components/AboutPage/AboutPage.jsx
@@ -1,23 +1,22 @@
 import React from 'react';
+import { Container, Typography } from '@mui/material';
 
-// This is one of our simplest components
-// It doesn't have local state,
-// It doesn't dispatch any redux actions or display any part of redux state
-// or even care what the redux state is'
+// This is a simple component. It doesn't have local state, dispatch
+// any redux actions or display any part of redux state
 
 function AboutPage() {
   return (
-    <div className="container">
-      <div>
-        <p>Chickadee is an ‘un-social media’ web application - its goal is to foster intentionality and support users in the deepening of close-connections outside of the app, in real-life. In an ever global, fast-paced, and disconnected world, Chickadee is a place to share in the unique life events that matter most to you - so that you and your closest connections can celebrate one another, and what matters to each of you, in the most impactful way. </p>
-      </div>
-      <div>
-        <p>Users create a profile, indicating the ways in which they most prefer to receive care from their closest connections. Users add life events, or flights, that are most meaningful to them, then share them with their connections, known as their flock. Users are reminded of their flock’s upcoming flights, with prompts to encourage action outside of the web app itself</p>
-      </div>
-      <div>
-        <p>Chickadees are small songbirds that form complex social hierarchies and maintain strong friendships within their flock. Their distinctive call is often used to keep the flock together. Similarly, the Chickadee app is intended to keep connections close, no matter how far away they may be from the people that matter most to them.</p>
-      </div>
-    </div>
+    <Container>
+      <Container>
+        <Typography>Chickadee is an ‘un-social media’ web application - its goal is to foster intentionality and support users in the deepening of close-connections outside of the app, in real-life. In an ever global, fast-paced, and disconnected world, Chickadee is a place to share in the unique life events that matter most to you - so that you and your closest connections can celebrate one another, and what matters to each of you, in the most impactful way.</Typography>
+      </Container>
+      <Container>
+        <Typography>Users create a profile, indicating the ways in which they most prefer to receive care from their closest connections. Users add life events, or flights, that are most meaningful to them, then share them with their connections, known as their flock. Users are reminded of their flock’s upcoming flights, with prompts to encourage action outside of the web app itself</Typography>
+      </Container>
+      <Container>
+        <Typography>Chickadees are small songbirds that form complex social hierarchies and maintain strong friendships within their flock. Their distinctive call is often used to keep the flock together. Similarly, the Chickadee app is intended to keep connections close, no matter how far away they may be from the people that matter most to them.</Typography>
+      </Container>
+    </Container>
   );
 }
 
--- src/components/MyNest/MyNest.jsx
+++ src/components/MyNest/MyNest.jsx
@@ -1,4 +1,37 @@
- function MyNest(){}
+import { Container, Typography, Input, TextField, } from "@mui/material"; 
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import { DatePicker, LocalizationProvider } from '@mui/x-date-pickers';
+
+
+
+ function MyNest(){
+
+return (
+<>
+    <Container sx={{bgcolor:'plum', height:'100'}}>
+        <Typography variant="h1" textAlign="center" sx={{":hover": {bgcolor: "#6D7D98"}}}>My Nest</Typography>
+    </Container>
+
+    <Container>
+    <form>
+        <TextField label="Name"/>
+    </form>
+    <form>
+        <TextField label="Pronouns"/>
+    </form>
+    <Container components={['DatePicker']}>
+        <LocalizationProvider dateAdapter={AdapterDayjs}>
+            <DatePicker label="Birthday" />
+        </LocalizationProvider>
+    </Container>
+
+
+
+</Container>
+</>
+
+)}
+
 
 
  export default MyNest; 
