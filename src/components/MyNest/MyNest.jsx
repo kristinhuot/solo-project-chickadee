9c70a31edf9a4034a7b2971223247032ae3da771
Kristin Huot <kristin.huot@gmail.com>
2024-05-21 18:19:54 -0500

    completed inputs, rendering as expected

--- src/components/MyNest/MyNest.jsx
+++ src/components/MyNest/MyNest.jsx
@@ -4,12 +4,13 @@ import { DatePicker, LocalizationProvider } from '@mui/x-date-pickers';
 import { useState } from "react";
 import { useHistory } from "react-router-dom/cjs/react-router-dom.min";
 import { useDispatch } from "react-redux";
+import dayjs from "dayjs";
 
  function MyNest(){
 
 const [name, setName] = useState('')
 const [pronouns, setPronouns] = useState('')
-const [birthday, setBirthday] = useState()
+const [birthday, setBirthday] = useState(dayjs())
 const [location, setLocation] = useState('')
 const [photo, setPhoto] = useState('')
 const history = useHistory()
@@ -21,7 +22,7 @@ const submitMyNestInputs = () => {
         type: 'SUBMIT_MYNEST_INPUTS',
         payload: name, pronouns, birthday, location, photo
     })
-
+    history.push('my_care_preferences')
 }
 
 return (
@@ -32,19 +33,35 @@ return (
 
     <Container>
     <form>
-        <TextField onChange={(e) => setName(e.target.value)} value={name} label="Name"/>
+        <TextField 
+        onChange={(e) => setName(e.target.value)}
+        value={name} 
+        label="Name"/>
     </form>
     <form>
-        <TextField onChange={(e) => setPronouns(e.target.value)} value={pronouns} label="Pronouns"/>
+        <TextField 
+        onChange={(e) => setPronouns(e.target.value)} 
+        value={pronouns} 
+        label="Pronouns"
+        />
     </form>
         <LocalizationProvider dateAdapter={AdapterDayjs}>
-            <DatePicker onChange={(e) => setBirthday(e.target.value)} value={birthday} label="Birthday" />
+            <DatePicker 
+            onChange={(newValue) => setBirthday(newValue)}
+            value={birthday} 
+            label="Birthday" />
         </LocalizationProvider>
     <form>
-        <TextField onChange={(e) => setLocation(e.target.value)} value={location} label="Location"/>
+        <TextField 
+        onChange={(e) => setLocation(e.target.value)}
+        value={location} 
+        label="Location"/>
     </form>
     <form>
-        <TextField onChange={(e) => setPhoto(e.target.value)} value={photo} label="Profile Photo URL"/>
+        <TextField 
+        onChange={(e) => setPhoto(e.target.value)} 
+        value={photo} 
+        label="Profile Photo URL"/>
     </form>
     <Button onClick={submitMyNestInputs} variant="contained">Preferences</Button>
 
