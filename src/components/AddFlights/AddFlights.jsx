db64bbb330f12b5c06f8742017d875ccf0e55de6
Kristin Huot <kristin.huot@gmail.com>
2024-05-26 21:28:58 -0500

    fixed modal styling and updating funcationality for the no button to return the user to home

--- src/components/AddFlights/AddFlights.jsx
+++ src/components/AddFlights/AddFlights.jsx
@@ -4,6 +4,7 @@ import { useState } from "react";
 import dayjs from "dayjs";
 import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 import { useDispatch } from "react-redux";
+import { useHistory } from "react-router-dom/cjs/react-router-dom.min";
 
 // Adds a new flight to the database and renders on the homepage 
 function AddFlights(){
@@ -12,7 +13,36 @@ const [flightTitle, setFlightTitle] = useState('');
 const [flightDetails, setFlightDetails] = useState('');
 const [flightDate, setFlightDate] = useState(dayjs())
 const dispatch = useDispatch()
+const history = useHistory()
+
+// Modal funcationality 
 const [showModal, setShowModal] = useState(false); // sets base state of modal to false so it does not yet appear 
+
+const handleModalCloseNo = () => {
+    setShowModal(false); // Close the modal
+    // Reset form fields 
+    history.push('/home')
+  };
+
+  const handleModalCloseYes = () => {
+    setShowModal(false); // Close the modal
+    // Reset form fields 
+    setFlightTitle('');
+    setFlightDetails('');
+    setFlightDate(dayjs());
+  };
+
+  const style = {
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    width: 400,
+    bgcolor: 'background.paper',
+    border: '2px solid #000',
+    boxShadow: 24,
+    p: 4,
+  };
 
 const submitNewFlight = () => {
 
@@ -23,14 +53,6 @@ const submitNewFlight = () => {
     setShowModal(true) // opens the modal after submitting a flight to see 
     // if the user wants to add another flight 
 }
-
-const handleModalClose = () => {
-    setShowModal(false); // Close the modal
-    // Reset form fields 
-    setFlightTitle('');
-    setFlightDetails('');
-    setFlightDate(dayjs());
-  };
 
 return (
 
@@ -53,6 +75,7 @@ return (
             value={flightDetails} 
             label="Flight Details"
             margin="dense"
+            multiline
             />
         </form>
             <LocalizationProvider dateAdapter={AdapterDayjs}>
@@ -69,11 +92,11 @@ return (
          <Button onClick={submitNewFlight} variant="contained">Submit Flight</Button>
     </Container>
 
-    <Modal open={showModal} onClose={handleModalClose}>
-        <Box>
-          <Typography variant="h6">Do you want to submit another flight?</Typography>
-          <Button onClick={handleModalClose}>No</Button>
-          <Button onClick={handleModalClose}>Yes</Button>
+    <Modal open={showModal} onClose={handleModalCloseYes}>
+        <Box sx={style}>
+          <Typography id="modal-submit-another-flight?" variant="h6">Do you want to submit another flight?</Typography>
+          <Button onClick={handleModalCloseNo}>No</Button>
+          <Button onClick={handleModalCloseYes}>Yes</Button>
         </Box>
       </Modal>
 </Container>
