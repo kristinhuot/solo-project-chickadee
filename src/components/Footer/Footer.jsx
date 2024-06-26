a0267cb631bc92332af57a06a40a02bf567d547a
Kristin Huot <151860024+kristinhuot@users.noreply.github.com>
2024-06-25 19:51:39 -0500

    Update Footer.jsx

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -7,7 +7,7 @@ import './Footer.css';
 // or even care what the redux state is, so it doesn't need 'connect()'
 
 function Footer() {
-  return <footer>&copy; Kristin Huot</footer>;
+  return <footer>&copy; Kristin Huot all rights reserved</footer>;
 }
 
 export default Footer;
